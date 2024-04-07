c33937ce10bc8e9f142b8a4d8d7f9dd0e1fa8df4
amtenu <amannov21@gmail.com>
2024-04-06 19:53:24 -0600

    Add reservation slice and configured with Redux store - Created reservation slice using createSlice from @reduxjs/toolkit - Integrated reservationSlice reducer into the Redux store reducer

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationSlice from "./features/reservationSlice";
 
-export const store=configureStore({
-    reducer:{
+export const store = configureStore({
+  reducer: {
+    reservationSlice,
+  },
+});
 
-    }
-})
-
-
-export type RootState=ReturnType<typeof store.getState>
-export type AppDispatch=typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
